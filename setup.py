c028c21ed5c05a4704ee3b93ecf1b4e77f42854a
Issa <issadillo130@gmail.com>
2024-01-01 14:41:24 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="botai",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Include any dependencies here
+    ],
+)
